db5fa91970c8895a72b7cc3166fe966099275d64
glazok28 <36017745+glazok28@users.noreply.github.com>
2018-02-06 08:34:26 +0300

    Add files via upload

--- homework/1.2/chat.js
+++ homework/1.2/chat.js
@@ -1,3 +1,6 @@
+// Доп. задание 2 - разбить на модули
+// сделаю после проверки частей 1,2 оснновного задания и ч.1 доп. задания
+
 const EventEmitter = require('events');
 
 class ChatApp extends EventEmitter {
@@ -14,6 +17,11 @@ class ChatApp extends EventEmitter {
       this.emit('message', `${this.title}: ping-pong`);
   }, 1000);
   }
+
+//ДЗ часть 2 - добавляем метод close
+  close(){
+    this.emit('close', {});
+  }
 }
 
 let webinarChat =  new ChatApp('webinar');
@@ -29,24 +37,30 @@ let prepareAnswer = () => {
   console.log('Готовлюсь к ответу');
 }
 
+let handleClose = () => {
+  console.log('Чат вконтакте закрылся :(');
+}
+
 webinarChat.on('message', chatOnMessage);
 
-//ДЗ часть 1 - добавляем обработчик события, который выводит 'Готовлюсь к ответу'
+//ДЗ часть 1 - добавляем обработчик события, который выводит 'Готовлюсь к ответу' к webinarChat
 webinarChat.on('message', prepareAnswer);
 
 facebookChat.on('message', chatOnMessage);
 
 //ДЗ часть 1 - устанавливаем максимальное количество обработчиков событий, равное 2
 vkChat.setMaxListeners(2);
+
 vkChat.on('message', chatOnMessage);
 
-//ДЗ часть 1 - добавляем обработчик события, который выводит 'Готовлюсь к ответу'
+//ДЗ часть 1 - добавляем обработчик события, который выводит 'Готовлюсь к ответу' к vkChat
 vkChat.on('message', prepareAnswer);
 
-// vkChat.on('message', chatOnMessage);
-// vkChat.on('message', chatOnMessage);
-// vkChat.on('message', chatOnMessage);
+//ДЗ часть 2 - добавляем обработчик close
+vkChat.on('close', handleClose);
 
+//ДЗ часть 2 - вызываем метод close
+vkChat.close();
 
 // Закрыть вконтакте
 setTimeout( ()=> {
@@ -54,9 +68,17 @@ setTimeout( ()=> {
 vkChat.removeListener('message', chatOnMessage);
 }, 10000 );
 
-
 // Закрыть фейсбук
 setTimeout( ()=> {
   console.log('Закрываю фейсбук, все внимание — вебинару!');
 facebookChat.removeListener('message', chatOnMessage);
 }, 15000 );
+
+
+// ДЗ доп - отписать  chatOnMessage от вебинара webinarChat через 30 сек
+// это имелось ввиду ?
+setTimeout( ()=> {
+  console.log('Закрываю вебинар...');
+webinarChat.removeListener('message', chatOnMessage);
+webinarChat.removeListener('message', prepareAnswer);
+}, 30000 );
